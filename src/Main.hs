c1fe0165ef4121eb83c24642c35cc5a6562d6800
Anton Kalyaev <a.kalyaev@fun-box.ru>
2015-04-25 19:00:31 +0300

    remove window and form menu items

--- src/Main.hs
+++ src/Main.hs
@@ -11,45 +11,19 @@ main :: IO ()
 main = do
     _ <- initGUI
 
-    window      <- windowNew
-    table       <- tableNew 1 5 False
-    sw          <- scrolledWindowNew Nothing Nothing
-    contents    <- textViewNew
-    statusBar   <- statusbarNew
     actionGroup <- actionGroupNew ("AppActions" :: Text)
     uim         <- uiManagerNew
-    indicator   <- appIndicatorNew ("example-simple-client" :: Text)
+    indicator   <- appIndicatorNew ("mike-indicator" :: Text)
                                    ("indicator-messages" :: Text)
                                    AppIndicatorCategoryApplicationStatus
 
-    set window [ windowTitle := ("Indicator Demo" :: Text)
-               , windowIconName := ("indicator-messages-new" :: Text)
-               ]
-    _ <- on window objectDestroy mainQuit
-    containerAdd window table
-
     actionGroupAddActions actionGroup entries
     uiManagerInsertActionGroup uim actionGroup 0
-    _ <- windowAddAccelGroup window `fmap` uiManagerGetAccelGroup uim
     catch (uiManagerAddUiFromString uim uiInfo >> return ())
           (\e -> putStrLn $ "Failed to build menus: "
                          ++ show (e :: SomeException))
-    Just menuBar <- uiManagerGetWidget uim ("/ui/MenuBar" :: Text)
-    widgetShow menuBar
-    tableAttach table menuBar 0 1 0 1 [Expand, Fill] [] 0 0
 
-    scrolledWindowSetPolicy sw PolicyAutomatic PolicyAutomatic
-    scrolledWindowSetShadowType sw ShadowIn
-    tableAttach table sw 0 1 3 4 [Expand, Fill] [Expand, Fill] 0 0
-    windowSetDefaultSize window 200 200
-    widgetGrabFocus contents
-    containerAdd sw contents
-
-    tableAttach table statusBar 0 1 4 5 [Expand, Fill] [] 0 0
-
-    widgetShowAll window
-
-    Just indicatorMenu <- uiManagerGetWidget uim ("/ui/IndicatorPopup" :: Text)
+    Just indicatorMenu <- uiManagerGetWidget uim ("/ui/MenuBar" :: Text)
     set indicator [appIndicatorAttentionIconName :=
                        Just ("indicator-messages-new" :: Text)
                   ]
@@ -60,39 +34,44 @@ main = do
 
 entries :: [ActionEntry]
 entries = [
-    ActionEntry "FileMenu" "_File" Nothing
+    ActionEntry "X:Y until Rest" "_X:Y until Rest" (Just "time-until-rest")
                  Nothing Nothing
-                 (return ()),
-    ActionEntry "New" "_New" (Just "document-new")
-                 (Just "<control>N") (Just "Create a new file")
-                 (activateAction "New"),
-    ActionEntry "Open" "_Open" (Just "document-open")
-                 (Just "<control>D") (Just "Open a file")
                  (activateAction "Open"),
-    ActionEntry "Save" "_Save" (Just "document-save")
-                 (Just "<control>S") (Just "Save file")
-                 (activateAction "Save"),
+    ActionEntry "Pause Timer" "_Pause Timer" (Just "pause-timer")
+                 Nothing Nothing
+                 (activateAction "Open"),
+    ActionEntry "Restart Timer" "_Restart Timer" (Just "restart-timer")
+                 Nothing Nothing
+                 (activateAction "Open"),
+    ActionEntry "Ready to Rest" "_Ready to Rest" (Just "ready-to-rest")
+                 Nothing Nothing
+                 (activateAction "Open"),
+    ActionEntry "Settings" "_Settings" (Just "settings")
+                 Nothing Nothing
+                 (activateAction "Open"),
+    ActionEntry "Show Time in Menu Bar" "_Show Time in Menu Bar" (Just "show-time-in-menu-bar")
+                 Nothing Nothing
+                 (activateAction "Open"),
+    ActionEntry "Show Animated Progress in Menu Bar" "_Show Animated Progress in Menu Bar" (Just "show-animated-progress-in-menu-bar")
+                 Nothing Nothing
+                 (activateAction "Open"),
     ActionEntry "Quit" "_Quit" (Just "application-exit")
-                 (Just "<control>Q") (Just "Exit the application")
+                 Nothing Nothing
                  mainQuit
     ]
 
 uiInfo :: Text
 uiInfo = "<ui>\
-         \  <menubar name='MenuBar'>\
-         \    <menu action='FileMenu'>\
-         \      <menuitem action='New'/>\
-         \      <menuitem action='Open'/>\
-         \      <menuitem action='Save'/>\
-         \      <separator/>\
-         \      <menuitem action='Quit'/>\
-         \    </menu>\
-         \  </menubar>\
-         \  <popup name='IndicatorPopup'>\
-         \    <menuitem action='New' />\
-         \    <menuitem action='Open' />\
-         \    <menuitem action='Save' />\
-         \    <menuitem action='Quit' />\
+         \  <popup name='MenuBar'>\
+         \    <menuitem action='X:Y until Rest'/>\
+         \    <menuitem action='Pause Timer'/>\
+         \    <menuitem action='Restart Timer'/>\
+         \    <menuitem action='Ready to Rest'/>\
+         \    <separator/>\
+         \    <menuitem action='Settings'/>\
+         \    <menuitem action='Show Time in Menu Bar'/>\
+         \    <menuitem action='Show Animated Progress in Menu Bar'/>\
+         \    <menuitem action='Quit'/>\
          \  </popup>\
          \</ui>"
 
